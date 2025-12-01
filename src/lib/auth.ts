e193c5a1f729f6f714487008dceef54375bd2e4d
Kiki <killian.sofiane@gmail.com>
2025-12-01 03:08:56 +0100

    fix(auth): remove custom scope to debug 403

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,11 +12,6 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID || process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_SECRET || process.env.GITHUB_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          scope: "read:user user:email repo",
-        },
-      },
     }),
   ],
   callbacks: {

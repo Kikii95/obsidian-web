684d3a05f0003e17d4a6d33e36f5ef9dbed3ee36
Kiki <killian.sofiane@gmail.com>
2025-12-01 03:20:09 +0100

    fix(auth): add repo scope back for vault access

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,6 +12,11 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID || process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_SECRET || process.env.GITHUB_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: "repo",
+        },
+      },
     }),
   ],
   callbacks: {
